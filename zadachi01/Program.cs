01bcdf181db0931cde7b8d6b47a230e97e2183c6
Ирина Бакунович <pasechnekova@mail.ru>
2023-01-25 12:38:35 +0300

    Решение

--- zadachi01/Program.cs
+++ zadachi01/Program.cs
@@ -3,3 +3,31 @@
  14212->нет
  12821->да
  23432->да*/
+
+Console.Clear();
+void CheckPalindromicNumber(int number)
+{
+    if(number >= 10000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+ 
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int division2 = number / 100;
+                int remainder2 = number % 10;
+                if(division2 == remainder2)
+                    Console.WriteLine("Да");
+            }
+            else 
+            Console.WriteLine("Нет");
+            
+    }
+    else
+    Console.WriteLine("Некорректное число!");
+}
+ 
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
